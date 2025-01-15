1d980e86ed800e135775c2f3c0cb008a3353d5cf
Vynzter <vynraven@gmail.com>
2025-01-15 11:37:06 +0700

    Portofolio Website

---
+++ Portofolio Website/script.js
@@ -0,0 +1,24 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+    var i;
+    var imgList = document.getElementsByClassName("img-slideshow");
+
+    if (n > imgList.length)  slideIndex = 1; 
+    if (n < 1) slideIndex = imgList.length; 
+
+    for (i = 0; i < imgList.length; i++) {
+        imgList[i].style.display = "none"; // Sembunyikan semua gambar
+    }
+    imgList[slideIndex - 1].style.display = "block"; // Tampilkan gambar yang sesuai
+}
+
+// Auto-slide setiap 3 detik
+setInterval(() => {
+    plusDivs(1);
+}, 3000);
